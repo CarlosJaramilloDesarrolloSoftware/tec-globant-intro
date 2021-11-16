d2ea76717ca487c6a97a0d93e224079dab16836c
CarlosJaramilloDesarrolloSoftware <docentecesde2019@gmail.com>
2021-11-16 12:11:36 -0500

    DOM validations with JS

--- Bootstrap/js/example1.js
+++ Bootstrap/js/example1.js
@@ -2,7 +2,7 @@ let names = prompt('Ingrese el nombre')
 let subjects = prompt('Ingrese cantidad materias')
 let price = prompt('Ingrese valor cada materia')
 
-subtotal = subjects * price
+subtotal = Number(subjects) * Number(price)
 subtotal = subtotal * .8
 
 total = subtotal + 20000 + 8000
